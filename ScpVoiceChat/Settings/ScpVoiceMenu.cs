2eb3c657246d7d9b023e5e3f27498a638c5a1ccb
Mallifrey <mallifrey@gmail.com>
2025-06-01 18:48:58 +0200

    FIxed SettingsEntry instancing

--- ScpVoiceChat/Settings/ScpVoiceMenu.cs
+++ ScpVoiceChat/Settings/ScpVoiceMenu.cs
@@ -13,9 +13,9 @@ public class ScpVoiceMenu : SettingsMenu
     public override string CustomId { get; } = "scpVoiceMenu";
     public override string Header => ScpVoiceConfig.Instance.MenuLabel;
 
-    public ScpVoiceSizeSlider SizeSlider { get; } = new ScpVoiceSizeSlider();
-    public ScpVoiceOffsetSlider OffsetSlider { get; } = new ScpVoiceOffsetSlider();
-    public ScpVoiceAlignDropdown AlignDropdown { get; } = new ScpVoiceAlignDropdown();
+    public ScpVoiceSizeSlider SizeSlider => new ScpVoiceSizeSlider();
+    public ScpVoiceOffsetSlider OffsetSlider => new ScpVoiceOffsetSlider();
+    public ScpVoiceAlignDropdown AlignDropdown => new ScpVoiceAlignDropdown();
     
     public override void BuildMenu(List<SettingsEntry> settings)
     {
